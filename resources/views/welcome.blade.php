b10e6000a7c176350bd2567deb5f6b6845b14dde
Syed <dcsyedfaraz@gmail.com>
2024-10-22 17:33:24 +0500

    "Update title in welcome.blade.php from 'Milung' to 'MiLung-OrrderFlow'"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Milung</title>
+    <title>MiLung-OrrderFlow</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
